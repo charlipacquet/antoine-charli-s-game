2227b51bc6d16e10b0ec9e6aa4d853297bf2efbc
charli <charli.pacquet@gmail.com>
2019-11-27 15:15:06 +0100

    charli
    on as regler le bug de la duplication des cubes

--- Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
+++ Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
@@ -81,10 +81,7 @@ namespace KartGame.KartSystems
                 // Instantiate the projectile at the position and rotation of this transform
                 Rigidbody clone;
                 clone = Instantiate(projectile, transform.position, transform.rotation);
-
-                // Give the cloned object an initial velocity along the current
-                // object's Z axis
-                clone.velocity = transform.TransformDirection(Vector3.forward * 10);
+                
             }
         }
 
